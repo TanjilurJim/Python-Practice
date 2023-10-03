d316531e291d70356d63ccd3f6e7f2ef06d9e597
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2023-10-04 02:42:03 +0600

    Update pattern triangle.py

--- Beginner/pattern triangle.py
+++ Beginner/pattern triangle.py
@@ -1,10 +1,13 @@
 n= int(input("Please enter a number: "))
 
 for i in range (n):
-
-    for j in range(i+1,n):
-        print(end=" " "*")
-
-
+    for j in range(i+1):
+        print( "*", end="")
     print("")
+
+
+
+
+
+
 
