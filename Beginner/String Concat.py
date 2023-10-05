cc28d54f48a00aa8849e55399dacb7e2f0b7f3eb
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2023-10-05 22:57:49 +0600

    more practice

---
+++ Beginner/String Concat.py
@@ -0,0 +1,10 @@
+s1 = 'hello '
+s2 = 'world '
+
+print(s1,s2)
+
+str(15)
+
+s3 = s1 +s2 + str(15)
+
+print(s3)
---
+++ Beginner/indexing.py
@@ -0,0 +1,17 @@
+s = "Hello world "
+n = [1782139]
+
+# for i in range(0,len(s)):
+#     print(s[i])
+
+# for i in range(len(s)-1,-1,-1):
+#     print(s[i])
+
+# if 'h' in s:
+#     print("ok")
+#
+# else:
+#     print("not ok")
+
+if 4 not in n:
+    print("nai toh")
