d96bc7707612855fd2aba145b82f9139407fad78
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2023-10-05 22:27:45 +0600

    practice

---
+++ Beginner/String.py
@@ -0,0 +1,4 @@
+s2 = input("enter a string: ")
+print(len(s2))
+for i in s2:
+    print(i,end="")
---
+++ Beginner/farhan.py
@@ -0,0 +1,8 @@
+def show(a):
+    print(a)
+
+
+for i in range(5):
+     n = int(input("Please enter a number: "))
+
+show(n)
