abe49646bdc77dbe978c1b64f485a65ee3bcd2a2
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2023-10-30 18:49:12 +0600

    Update find_expense_and_related.py

--- DataStructure and Algorithms/Arrays Exercise/find_expense_and_related.py
+++ DataStructure and Algorithms/Arrays Exercise/find_expense_and_related.py
@@ -14,3 +14,26 @@
 # got a refund of 200$. Make a correction to your monthly expense list
 # based on this
 
+expenses = {'january':2000, 'february':2350, 'march':2650,'april':2130,'May':2190}
+
+exp_lst = [(k) for k in expenses.values()]
+
+# print(exp_lst)
+
+#1
+print("In february you spent:",exp_lst[1]-exp_lst[0], "more than januray")
+# 2
+print("Total spent in the first quarter is: ",sum(exp_lst[:3]))
+# 3
+if 2000 in exp_lst:
+  print("yes, 2000 is in the list", end=" ")
+  print("it is in ",exp_lst.index(2000),"th index")
+
+# 4
+# expenses.update({'June':1980})
+# print(expenses)
+exp_lst.append(1980)
+print(exp_lst)
+
+#5
+print("expenses after the return of 200 dollar of april: ", exp_lst[3]-200,"dollar")
