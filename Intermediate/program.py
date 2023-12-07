4d36ee29ea4287f887411d8b462333d6531fe35d
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2023-12-07 07:56:19 +0600

    new practices

---
+++ Intermediate/program.py
@@ -0,0 +1,7 @@
+
+import trial
+
+print("Hello, I am inside program.py!")
+print(trial.__name__)
+# n = fibo.find_fib(15)
+# print("15th fibonacci number is, ",n)
---
+++ Intermediate/trial.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    print("Hello from trial")
+# print("my name is", __name__)
