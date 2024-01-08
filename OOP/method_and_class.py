8eb73f819acc2e0728bbb5a6351bd5b7bfa2805d
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-01-09 05:21:45 +0600

    Update method_and_class.py

--- OOP/method_and_class.py
+++ OOP/method_and_class.py
@@ -8,12 +8,18 @@ class Car:
 
 
     def start(self):
+        print("name:", self.name)
+        print("color:", self.color)
         print("Starting the engine")
 
+
 my_car = Car("Allion", "White")
+my_car.start()
+my_car2 = Car("premio", "Navy Blue")
+my_car2.start()
 # my_car.name = "Allion"
 # my_car.color = "Blue"
 
-print(my_car.name)
-print(my_car.color)
-my_car.start()
+# print(my_car.name)
+# print(my_car.color)
+# my_car.start()
