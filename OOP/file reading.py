099f65fac74a6a66b2405ca2e321272fef6c797c
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-01-10 02:40:45 +0600

    more practice

---
+++ OOP/file reading.py
@@ -0,0 +1,4 @@
+file_handler = open("MyData.txt" ,'r')
+str1 = file_handler.read()
+print(str1)
+print(type(str1))
--- OOP/req_module.py
+++ OOP/req_module.py
@@ -5,3 +5,8 @@ url = "https://github.com/TanjilurJim"
 res = requests.get(url)
 
 print(res.text)
+
+# fp = open("test.text","w")
+# fp.write("This was created using python")
+#
+# fp.close()
