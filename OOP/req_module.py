8f5479610bf2685ed4c037088998104e804cb7e8
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-01-10 06:42:30 +0600

    more works with requests module

--- OOP/req_module.py
+++ OOP/req_module.py
@@ -1,10 +1,20 @@
 import requests
+import os
+import webbrowser as wb
 
-url = "https://github.com/TanjilurJim"
+
+url = "https://dimikoj.com/"
 
 res = requests.get(url)
 
-print(res.text)
+with open("dimik.html","w",encoding=res.encoding) as f:
+    f.write(res.text)
+
+file_path = os.path.realpath("dimik.html")
+print(file_path)
+
+wb.open("file://" + file_path)
+wb.open(url)
 
 # fp = open("test.text","w")
 # fp.write("This was created using python")
