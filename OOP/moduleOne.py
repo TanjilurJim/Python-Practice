993b44cbfb49ce6b87224624400491a74ce56945
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-02-05 18:52:33 +0600

    MODULE

---
+++ OOP/moduleOne.py
@@ -0,0 +1,13 @@
+
+total_amount = 2000
+
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+if __name__ =='__main__':
+    print(__name__)
+    print(add(20,30))
+    print(sub(50,10))
---
+++ OOP/myprogram.py
@@ -0,0 +1,10 @@
+import moduleOne as mo
+
+n1 = 10
+n2 = 20
+
+print('this is addition',mo.add(n1,n2))
+
+print(mo.sub(n2,n1))
+
+print(mo.total_amount)
