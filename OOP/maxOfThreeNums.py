0341eaebdb5a5ab0357bfdda7e87f0246c8b215c
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-02-06 13:19:36 +0600

    Create maxOfThreeNums.py

---
+++ OOP/maxOfThreeNums.py
@@ -0,0 +1,21 @@
+def max3(a,b,c):
+    max = 0
+    if a > b:
+        max = a
+    else:
+        max = b
+
+    if max < c:
+        max = c
+
+    return max
+
+inputs=[]
+for i in range(3):
+    number = int(input(f"Please enter the {i+1} number"))
+    inputs.append(number)
+
+maximum = max3(*inputs)
+
+print(maximum)
+
