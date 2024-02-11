19b0de7a1df7f25f6b6b31043e66df2d720cf521
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-02-12 01:10:14 +0600

    Create negative Age exception.py

---
+++ OOP/negative Age exception.py
@@ -0,0 +1,30 @@
+
+def stage(age):
+    try:
+        if age<0:
+            raise ValueError("Age cannot be negative.")
+
+
+
+        elif age < 13:
+            print(f'he is only {age} years old. He is a kid')
+        elif age >= 13 and age <19:
+            print(f'you are {age} hence you are a teen')
+        elif age >19 and age <=50:
+            print(f'you are {age} so still young')
+        else:
+            print(f'It is okay to be old. You are {age}')
+    except ValueError as e:
+        print(f"Error: {e} ")
+
+
+
+stage(12)
+stage(100)
+stage(0)
+stage(-8)
+
+
+
+
+
