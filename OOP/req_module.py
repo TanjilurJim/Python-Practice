4ce789d2d360e06414842f2c0d3e6b3c23e53583
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-02-13 16:44:12 +0600

    practice

--- OOP/req_module.py
+++ OOP/req_module.py
@@ -23,12 +23,29 @@ import webbrowser as wb
 
 # for downloading image
 
-img_url = "https://2.bp.blogspot.com/-zED0hpjBOKU/WSrodhLjLMI/AAAAAAAACpk/3Gv5YyxHsjErYphNfzFd37uEmxsBC0XtwCK4B/s1600/Screen%2BShot%2B2017-05-23%2Bat%2B10.48.44%2BPM.png"
-r = requests.get(img_url)
+# img_url = "https://2.bp.blogspot.com/-zED0hpjBOKU/WSrodhLjLMI/AAAAAAAACpk/3Gv5YyxHsjErYphNfzFd37uEmxsBC0XtwCK4B/s1600/Screen%2BShot%2B2017-05-23%2Bat%2B10.48.44%2BPM.png"
+# r = requests.get(img_url)
+#
+# with open("pybook.png", "wb") as f:
+#     f.write(r.content)
+#
+# fr = open("pybook.png","rb")
+# show_image = fr.read()
+# print(show_image)
 
-with open("pybook.png", "wb") as f:
-    f.write(r.content)
+# u = "https://tanjilurjdot.pythonanywhere.com/"
+#
+# response = requests.get(u)
+#
+# with open('jim.html','w') as f:
+#     f.write(response.text)
+#
+# file_path = os.path.realpath('jim.html')
+# print(file_path)
+# wb.open("file://" + file_path)
 
-fr = open("pybook.png","rb")
-show_image = fr.read()
-print(show_image)
+i_url = "https://images.pexels.com/photos/1133505/pexels-photo-1133505.jpeg?cs=srgb&dl=asphalt-blue-clouds-1133505.jpg&fm=jpg"
+
+image_req = requests.get(i_url)
+with open('aesthetic.png', 'wb') as f:
+    f.write(image_req.content)
