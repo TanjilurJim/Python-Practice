bc10461abe52ed9e7677952799d1c106c218d5a2
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-02-17 03:03:43 +0600

    constructor in inheritance
    learned how to call the parent class instructor in child class

---
+++ OOP/cuboid.py
@@ -0,0 +1,1 @@
+class Cuboid(Rectangle):
--- OOP/rectangle.py
+++ OOP/rectangle.py
@@ -10,11 +10,28 @@ class Rectangle():
     def area(self):
         return self.length * self.breadth
     
-    @staticmethod
-    def issquare(lent,bre):
-        return lent ==bre
+    # @staticmethod
+    # def issquare(lent,bre):
+    #     return lent ==bre
+
+
+class Cuboid(Rectangle):
+    def __init__(self, length, breadth,h):
+        self.height = h
+
+        super().__init__(length, breadth)
+    def volume(self):
+        return self.length * self.breadth * self.height
     
 
-r1 = Rectangle(10,5)
+    
 
-print(r1.issquare(10,10))
+# r1 = Rectangle(10,5)
+# print(r1.area())
+# print(r1.perimeter())
+
+# print(r1.issquare(10,10)) 
+    
+c = Cuboid(10,5,3)
+
+print(c.volume())
