835bcebb9ff5bb5bb20bf8d73b99d07b8b40639d
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-02-18 04:28:08 +0600

    ok

---
+++ OOP/MethodOverloading.py
@@ -0,0 +1,14 @@
+class Arith():
+    def summation(self,a,b,c=None):
+        s =a +b
+        if c == None:
+            return s
+        else:
+            return s+c
+        
+
+a = Arith()
+
+print(a.summation(10,23.4))
+print(a.summation(10,12,13))
+
