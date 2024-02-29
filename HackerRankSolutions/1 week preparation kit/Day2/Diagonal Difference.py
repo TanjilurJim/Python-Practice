ad9d53e29e8a0fada76254cd916f0a05be430cd7
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-02-29 22:58:48 +0600

    Create Diagonal Difference.py

---
+++ HackerRankSolutions/1 week preparation kit/Day2/Diagonal Difference.py
@@ -0,0 +1,19 @@
+'''given array = 1 2 3
+                 4 5 6
+                 9 8 9
+
+absolutte difference will be 1+5+9 - 3+5+9'''
+
+
+arr = [[1,2,3] ,[4,5,6]]
+# print("enter the elements of a #x# matrix, one row at a time")
+#
+# for i in range(3):
+#     row = input(f"Row {i+1}: ").split()
+#     arr.append([int(x) for x in row])
+#
+# # print("row")
+# for row in arr:
+#     print(row)
+
+print(arr[1][2] )
